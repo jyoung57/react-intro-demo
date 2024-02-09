4334af93d1e19349a3599aad8ee671b0506358b0
Jon <jyoung57@W-BC91R73>
2024-02-08 20:01:06 -0600

    Added header/main/footer jsx files and imported to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import './App.css';
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <>
-      <header>
-        <h1>Intro to React</h1>
-      </header>
-      <main>
-        <p>
-          This is an intro to react Lorem, ipsum dolor sit amet consectetur
-          adipisicing elit. Libero ipsa impedit laboriosam, voluptatibus
-          repudiandae tempore, architecto porro harum sunt quo expedita, labore
-          temporibus? Quod saepe consequatur dolorum officiis, nostrum iusto.
-          Fugiat repellendus enim corrupti esse modi excepturi ipsa ea mollitia,
-          fugit labore tempora eaque possimus deleniti libero illo praesentium
-          perferendis doloribus illum aliquam autem! Enim accusantium harum
-          incidunt deleniti. Tempore.
-        </p>
-      </main>
-      <footer>&copy; Codex Academy</footer>
+      <Header/>
+      <Main/>
+      <Footer/>
     </>
   );
 }
