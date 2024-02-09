527893e2d34c3f3949426e6af6c4e8e65a448efe
Jon <jyoung57@W-BC91R73>
2024-02-08 19:59:46 -0600

    added component folder with Header.jsx, Footer.jsx and Main.jsx

---
+++ src/Components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <main>
+      <p>
+        This is an intro to react Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Libero ipsa impedit laboriosam, voluptatibus
+        repudiandae tempore, architecto porro harum sunt quo expedita, labore
+        temporibus? Quod saepe consequatur dolorum officiis, nostrum iusto.
+        Fugiat repellendus enim corrupti esse modi excepturi ipsa ea mollitia,
+        fugit labore tempora eaque possimus deleniti libero illo praesentium
+        perferendis doloribus illum aliquam autem! Enim accusantium harum
+        incidunt deleniti. Tempore.
+      </p>
+    </main>
+  );
+}
