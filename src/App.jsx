42e961f61b3fa735ed4d27c3b98a89be6fecb1fa
Jon <jyoung57@W-BC91R73>
2024-02-08 19:51:04 -0600

    added header,main,footer and some sample information

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Intro to React</h1>
+      </header>
+      <main>
+        <p>
+          This is an intro to react Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Libero ipsa impedit laboriosam, voluptatibus
+          repudiandae tempore, architecto porro harum sunt quo expedita, labore
+          temporibus? Quod saepe consequatur dolorum officiis, nostrum iusto.
+          Fugiat repellendus enim corrupti esse modi excepturi ipsa ea mollitia,
+          fugit labore tempora eaque possimus deleniti libero illo praesentium
+          perferendis doloribus illum aliquam autem! Enim accusantium harum
+          incidunt deleniti. Tempore.
+        </p>
+      </main>
+      <footer>&copy; Codex Academy</footer>
+    </>
+  );
+}
+
+export default App;
