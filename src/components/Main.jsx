7a800ca0ae08f1c3683ee4a72c0eeddda870966e
Jon <jyoung57@W-BC91R73>
2024-02-10 14:29:15 -0600

    example React components, conditional rendering and state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import "../styles/Main.css";
+import '../styles/Main.css';
+import { useState } from 'react';
 
 export default function Main() {
+  const fruit = 'Blueberry';
+
+  const age = 20;
+
+  const someFruits = ['blueberry', 'apple', 'banana'];
+
+  const [count, setCount] = React.useState(0);
+
+  function handleButtonClick () {
+    setCount(count+1)
+    console.log(setCount)
+  }
+
   return (
     <main>
+      {fruit}
+      <br />
+      {age >= 21 ? 'Beer' : 'Juice'}
+      <br />
+      {(fruit == 'Blueberry' && 'Blue Berry Time') || 'null'}
+      <br />
+      {someFruits.map((fruit, index) => <li key={index}>{fruit}</li>)}
+      <br />
+
+      <p>Counter is: {count}</p>
+      <button onClick={handleButtonClick}>Click Me</button>
+
       <p>
         This is an intro to react Lorem, ipsum dolor sit amet consectetur
         adipisicing elit. Libero ipsa impedit laboriosam, voluptatibus
@@ -15,5 +41,5 @@ export default function Main() {
         incidunt deleniti. Tempore.
       </p>
     </main>
-  )
+  );
 }
